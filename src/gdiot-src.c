7f4b4c16ebc0bf4bab3ea5aff39eba8b6b2bb09b
ChrisG55 <christian_g_55@msn.com>
2021-02-11 14:33:29 +0100

    src: add argument parser to source node
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-src.c
+++ src/gdiot-src.c
@@ -2,9 +2,17 @@
 /*
  * Generalized deduplication IoT source.
  */
+#include "args.h"
+
 #include <stdlib.h>
+
+static const char *help_line = "GDIoT source node reference implementation";
 
 int main(int argc, char *argv[])
 {
+	conf.help_line = help_line;
+
+	parse_args(argc, argv);
+
 	return EXIT_SUCCESS;
 }
