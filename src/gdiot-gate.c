2df529637a1b04b635f475310e3543374aee9b51
ChrisG55 <christian_g_55@msn.com>
2021-02-11 14:33:29 +0100

    src: add argument parser to intermediate node
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-gate.c
+++ src/gdiot-gate.c
@@ -2,9 +2,17 @@
 /*
  * Generalized deduplication IoT gateway.
  */
+#include "args.h"
+
 #include <stdlib.h>
+
+static const char *help_line = "GDIoT intermediate node reference implementation";
 
 int main(int argc, char *argv[])
 {
+	conf.help_line = help_line;
+
+	parse_args(argc, argv);
+
 	return EXIT_SUCCESS;
 }
