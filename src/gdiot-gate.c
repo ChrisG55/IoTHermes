ed5a508f3c2ba37cf40786e7d4562f92aeaa404d
ChrisG55 <christian_g_55@msn.com>
2021-02-11 14:33:29 +0100

    src: add intermediate node reference implementation
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

---
+++ src/gdiot-gate.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Generalized deduplication IoT gateway.
+ */
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	return EXIT_SUCCESS;
+}
