4bc080859a47806d0e67fae19a93c7e675978194
ChrisG55 <christian_g_55@msn.com>
2021-02-18 15:03:10 +0100

    src: add server as task
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-gate.c
+++ src/gdiot-gate.c
@@ -3,26 +3,40 @@
  * Generalized deduplication IoT gateway.
  */
 #include "args.h"
+#include "exit.h"
+#include "server.h"
+#include "task.h"
 
+#include <errno.h>
 #include <stdlib.h>
-
-#include "lib/hermes.h"
 
 static const char *help_line = "GDIoT intermediate node reference implementation";
 
 int main(int argc, char *argv[])
 {
-	struct hermes_context *ctx;
+	int rc;
+	int rv = EXIT_SUCCESS;
+	int *src;
+	task_t server;
+	struct server_context sctx;
 
 	conf.help_line = help_line;
 
 	parse_args(argc, argv);
 
-	ctx = hermes_init();
-	hermes_connect(ctx);
-	hermes_send(ctx, NULL);
-	hermes_disconnect(ctx);
-	hermes_fini(ctx);
+	if ((rc = task_create(server_main, &server, (void *)&sctx)) != 0) {
+		errno = rc;
+	        return errno2exit();
+	}
 
-	return EXIT_SUCCESS;
+	if ((rc = task_join(server, (void **)&src)) != 0) {
+		errno = rc;
+		rv = errno2exit();
+	} else if (*src != 0) {
+		rv = EXIT_FAILURE;
+	}
+
+	task_free(server);
+
+	return rv;
 }
