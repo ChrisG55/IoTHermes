0454ef7a3e100898aec69e5d804f93688bd954b6
ChrisG55 <christian_g_55@msn.com>
2021-06-18 15:21:56 +0200

    src: add Hermes protocol library support to source node
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-src.c
+++ src/gdiot-src.c
@@ -6,13 +6,23 @@
 
 #include <stdlib.h>
 
+#include "lib/hermes.h"
+
 static const char *help_line = "GDIoT source node reference implementation";
 
 int main(int argc, char *argv[])
 {
+	struct hermes_context *ctx;
+
 	conf.help_line = help_line;
 
 	parse_args(argc, argv);
+
+	ctx = hermes_init();
+	hermes_connect(ctx);
+	hermes_send(ctx, NULL);
+	hermes_disconnect(ctx);
+	hermes_fini(ctx);
 
 	return EXIT_SUCCESS;
 }
