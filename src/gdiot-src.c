021ce63e88c18a8d4bba288ae0d0f55ea06f4800
ChrisG55 <christian_g_55@msn.com>
2021-07-16 18:19:19 +0200

    src: add client as task
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-src.c
+++ src/gdiot-src.c
@@ -3,26 +3,41 @@
  * Generalized deduplication IoT source.
  */
 #include "args.h"
+#include "client.h"
+#include "exit.h"
+#include "io.h"
+#include "task.h"
 
+#include <errno.h>
 #include <stdlib.h>
-
-#include "lib/hermes.h"
 
 static const char *help_line = "GDIoT source node reference implementation";
 
 int main(int argc, char *argv[])
 {
-	struct hermes_context *ctx;
+	int rc;
+	int rv = EXIT_SUCCESS;
+	int *crc;
+	task_t client;
+	struct client_context cctx;
 
 	conf.help_line = help_line;
 
 	parse_args(argc, argv);
 
-	ctx = hermes_init();
-	hermes_connect(ctx);
-	hermes_send(ctx, NULL);
-	hermes_disconnect(ctx);
-	hermes_fini(ctx);
+	if ((rc = task_create(client_main, &client, (void *)&cctx)) != 0) {
+		errno = rc;
+		return errno2exit();
+	}
 
-	return EXIT_SUCCESS;
+	if ((rc = task_join(client, (void **)&crc)) != 0) {
+		errno = rc;
+		rv = errno2exit();
+	} else if (*crc != 0) {
+		rv = EXIT_FAILURE;
+	}
+
+	task_free(client);
+
+	return rv;
 }
