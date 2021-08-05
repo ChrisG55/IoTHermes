f0e1583f882c525fbd40547c089b0e049fde36fc
ChrisG55 <christian_g_55@msn.com>
2021-08-05 08:25:43 +0200

    src: add client initialization message structure
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/queue_data.h
+++ src/queue_data.h
@@ -1,9 +1,29 @@
 #ifndef QUEUE_DATA_H
 #define QUEUE_DATA_H
 
+#include "config.h"
+#include "format.h"
+#include "queue.h"
+
+#if HAVE_NETDB_H
+#include <netdb.h>
+#else
+#error "system does not support addrinfo structure"
+#endif
+
 enum queue_data {
 	CLIENT_MESSAGE_DATA,
+	CLIENT_MESSAGE_INIT,
 	CLIENT_MESSAGE_RESPONSE,
+};
+
+struct client_msg_init {
+	const char *nodename;
+	const char *port;
+	struct addrinfo hints;
+	unsigned long id;
+	struct queue *queue;
+	struct format *format;
 };
 
 #endif /* QUEUE_DATA_H */
