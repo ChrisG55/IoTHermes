36ad84c7fd8bdfabbec3217cef6fdfbd19015300
ChrisG55 <christian_g_55@msn.com>
2021-08-30 08:19:49 +0200

    src: add server support for multiple clients
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/server.c
+++ src/server.c
@@ -1,6 +1,8 @@
+#include "hash.h"
 #include "queue.h"
 #include "queue_data.h"
 #include "server.h"
+#include "streamid.h"
 
 #include <errno.h>
 #include <stdint.h>
@@ -8,10 +10,26 @@
 
 #include "lib/hermes.h"
 
+struct client_context {
+	int fd;
+};
+
+static int server_init(struct server_context *ctx)
+{
+	ctx->registry = hash_create(UINT_MAX, streamid_compare, NULL);
+	if (ctx->registry == NULL)
+		return 1;
+
+	return 0;
+}
+
 void *server_main(void *c)
 {
 	struct server_context *ctx = c;
 	uint32_t *data;
+
+	if ((ctx->ret = server_init(ctx)) != 0)
+		return &ctx->ret;
 
 	if ((ctx->hermes = hermes_init(0, 0)) == NULL) {
 		ctx->ret = errno;
--- src/server.h
+++ src/server.h
@@ -1,6 +1,7 @@
 #ifndef SERVER_H
 #define sERVER_H
 
+#include "hash.h"
 #include "queue.h"
 
 #include "lib/hermes.h"
@@ -9,6 +10,7 @@ struct server_context {
 	int ret;
 	struct hermes_context *hermes;
 	struct queue *queue;
+	hash_t *registry;
 };
 
 void *server_main(void *c);
