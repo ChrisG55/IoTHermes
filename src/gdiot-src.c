7f3c6e0db67d3dba4b0a407afdf5599cd5cc3040
ChrisG55 <christian_g_55@msn.com>
2021-09-02 09:28:26 +0200

    src: implement source task for source node implementation
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-src.c
+++ src/gdiot-src.c
@@ -10,6 +10,7 @@
 #include "io.h"
 #include "queue.h"
 #include "queue_data.h"
+#include "source.h"
 #include "task.h"
 
 #include <errno.h>
@@ -17,17 +18,29 @@
 #include <stdlib.h>
 #include <time.h>
 
+#if HAVE_FCNTL_H
+#include <fcntl.h>
+#endif /* HAVE_FCNTL_H */
+
+#if HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif /* HAVE_SYS_STAT_H */
+
 static const char *help_line = "GDIoT source node reference implementation";
+
+struct source_context *sctx = NULL;
+int nb_sources = 0;
 
 int main(int argc, char *argv[])
 {
 	int rc;
 	int rv = EXIT_SUCCESS;
 	int *crc;
-	task_t client;
+	task_t client, source;
 	struct client_context cctx;
 	uint32_t *data;
 	struct queue queue;
+	char *fn;
 
 	conf.help_line = help_line;
 
@@ -50,6 +63,39 @@ int main(int argc, char *argv[])
 		return errno2exit();
 	}
 
+	if ((sctx = calloc(1, sizeof(*sctx))) == NULL)
+		return errno2exit();
+	nb_sources++;
+	if ((fn = calloc(9, sizeof(*fn))) == NULL)
+		return errno2exit();
+	fn[0] = 't';
+	fn[1] = 'e';
+	fn[2] = 's';
+	fn[3] = 't';
+	fn[4] = '.';
+	fn[5] = 'c';
+	fn[6] = 's';
+	fn[7] = 'v';
+	fn[8] = '\0';
+	sctx[0].type = SOURCE_CSV;
+#if HAVE_FCNTL_H && HAVE_SYS_STAT_H
+	sctx[0].fd = open(fn, O_RDONLY);
+#endif
+	sctx[0].queue = &queue;
+	sctx[0].client_queue = &cctx.source_queue;
+
+	if ((rc = task_create(source_main, &source, (void *)&sctx[0])) != 0) {
+		errno = rc;
+		return errno2exit();
+	}
+
+	if ((rc = task_join(source, (void **)&crc)) != 0) {
+		errno = rc;
+		rv = errno2exit();
+	} else if (*crc != 0) {
+		rv = EXIT_FAILURE;
+	}
+
 	if ((data = calloc(1, sizeof(*data))) == NULL)
 		return errno2exit();
 	*data = 0xc0ffee00;
@@ -67,6 +113,9 @@ int main(int argc, char *argv[])
 	}
 
 	task_free(client);
+	task_free(source);
+
+	free(fn);
 
 	return rv;
 }
--- src/gdiot-src.h
+++ src/gdiot-src.h
@@ -1,4 +1,10 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 #ifndef GDIOT_SRC_H
 #define GDIOT_SRC_H
+
+#include "source.h"
+
+extern struct source_context *sctx;
+extern int nb_sources;
+
 #endif /* GDIOT_SRC_H */
