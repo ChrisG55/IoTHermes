ea3d48a617bcf70ebe09cc9fd32f8fc56d2a30d4
ChrisG55 <christian_g_55@msn.com>
2021-10-20 10:58:03 +0200

    src: initialize pseudo-random number generator in sink node
    Signed-off-by: ChrisG55 <christian_g_55@msn.com>
    Tested-by: ChrisG55 <christian_g_55@msn.com>

--- src/gdiot-evap.c
+++ src/gdiot-evap.c
@@ -3,8 +3,10 @@
  * Generalized deduplication IoT evaporator.
  */
 #include "args.h"
+#include "config.h"
 
 #include <stdlib.h>
+#include <time.h>
 
 #include "lib/hermes.h"
 
@@ -18,7 +20,14 @@ int main(int argc, char *argv[])
 
 	parse_args(argc, argv);
 
+#if HAVE_UNISTD_H && (defined(_DEFAULT_SOURCE) || defined(_XOPEN_SOURCE))
+	srand48(time(NULL));
+#else
+	srand(time(NULL));
+#endif /* HAVE_UNISTD_H && (_DEFAULT_SOURCE || _XOPEN_SOURCE) */
+
 	ctx = hermes_init();
+
 	hermes_connect(ctx);
 	hermes_send(ctx, NULL);
 	hermes_disconnect(ctx);
